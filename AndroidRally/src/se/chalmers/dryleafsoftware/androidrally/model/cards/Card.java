001172bfa849b516851c2dc84d1a9bd667183a43
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-16 16:37:45 -0700

    Added getPriority and setter and getter for isLocked

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/cards/Card.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/cards/Card.java
@@ -2,9 +2,23 @@ package se.chalmers.dryleafsoftware.androidrally.model.cards;
 
 public abstract class Card {
 	private int priority;
+	private boolean isLocked;
 	
 	public Card(int priority) {
 		this.priority = priority; 
+		this.isLocked = false;
 	}
 	abstract public void action();
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public boolean isLocked() {
+		return isLocked;
+	}
+	
+	public void setIsLocked(boolean isLocked) {
+		this.isLocked = isLocked;
+	}
 }
