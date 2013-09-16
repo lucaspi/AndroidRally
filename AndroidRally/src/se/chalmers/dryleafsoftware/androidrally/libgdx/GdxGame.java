975d7ed58d8f1e61d9ef7e3dcfd1a13fd42373f0
Martin Calleberg <martin.calleberg@gmail.com>
2013-09-16 20:31:55 +0200

    Board display test

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/libgdx/GdxGame.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/libgdx/GdxGame.java
@@ -1,8 +1,10 @@
 package se.chalmers.dryleafsoftware.androidrally.libgdx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,14 +13,15 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GdxGame implements ApplicationListener {
 	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private ShapeRenderer shapeRenderer;
+	
+	private Texture texture;	// TODO: Move this
+	private List<Sprite> sprites = new ArrayList<Sprite>();	// TODO: Move this
 	
 	@Override
 	public void create() {	
@@ -28,6 +31,30 @@ public class GdxGame implements ApplicationListener {
 		camera.update();
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
+		
+		texture = new Texture(Gdx.files.internal("textures/testTile.png"));
+		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		TextureRegion factoryFloor = new TextureRegion(texture, 0, 0, 
+				texture.getWidth()/2, texture.getHeight());
+		TextureRegion dockFloor = new TextureRegion(texture, texture.getWidth()/2, 0, 
+				texture.getWidth()/2, texture.getHeight());
+		
+		// TODO: Move this
+		for(int x = 0; x < 12; x++) {
+			for(int y = 0; y < 12; y++) {
+				Sprite s = new Sprite(factoryFloor);
+				s.setSize(40, 40);
+				s.setPosition(40 * x, 760 - 40 * y);
+				sprites.add(s);
+			}
+			for(int y = 0; y < 4; y++) {
+				Sprite s = new Sprite(dockFloor);
+				s.setSize(40, 40);
+				s.setPosition(40 * x, 760 - 40 * y - 40 * 12);
+				sprites.add(s);
+			}
+		}
 	}
 
 	@Override
@@ -43,23 +70,10 @@ public class GdxGame implements ApplicationListener {
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();	
+		for(Sprite s : sprites) {
+			s.draw(batch);
+		}
 		batch.end();
-		
-		shapeRenderer.setProjectionMatrix(camera.combined);
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.line(0, 0, 480, 800);
-		shapeRenderer.end();
-		
-		shapeRenderer.begin(ShapeType.FilledRectangle);
-		shapeRenderer.setColor(Color.RED);
-		shapeRenderer.filledRect(50, 50, 480-100, 800-100);
-		shapeRenderer.end();
-		
-		shapeRenderer.begin(ShapeType.Circle);
-		shapeRenderer.setColor(Color.GREEN);
-		shapeRenderer.circle(240, 400, 100);
-		shapeRenderer.end();
 	}
 
 	@Override
