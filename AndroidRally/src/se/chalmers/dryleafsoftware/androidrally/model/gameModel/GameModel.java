dd7106a3a80210de475a7d58375ac840a95b2157
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-17 10:58:41 -0700

    Added method dealCards to GameModel

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
@@ -1,7 +1,9 @@
 package se.chalmers.dryleafsoftware.androidrally.model.gameModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import se.chalmers.dryleafsoftware.androidrally.model.cards.Card;
 import se.chalmers.dryleafsoftware.androidrally.model.cards.Deck;
 import se.chalmers.dryleafsoftware.androidrally.model.gameBoard.GameBoard;
 import se.chalmers.dryleafsoftware.androidrally.model.robots.Robot;
@@ -17,5 +19,18 @@ public class GameModel {
 		for (int i = 0; i < nbrOfPlayers; i++) {
 			robots.add(new Robot(i, 14));
 		}
+		deck = new Deck();
 	}
+	
+	public void dealCards() {
+		for(Robot robot : robots) {
+			int health = robot.getHealth();
+			List<Card> drawnCards = new ArrayList<Card>();
+			for (int i = 0; i < health; i++) {
+				drawnCards.add(deck.drawCard());
+			}
+			robot.addCards(drawnCards);
+		}
+	}
+	
 }
