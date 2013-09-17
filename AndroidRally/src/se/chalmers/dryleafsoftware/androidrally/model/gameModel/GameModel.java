566e504e2820ff14991b77a772f49f1cea29b0e5
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-17 10:44:52 -0700

    Added some basic functionality to GameModel

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
@@ -1,5 +1,21 @@
 package se.chalmers.dryleafsoftware.androidrally.model.gameModel;
 
-public class GameModel {
+import java.util.List;
 
+import se.chalmers.dryleafsoftware.androidrally.model.cards.Deck;
+import se.chalmers.dryleafsoftware.androidrally.model.gameBoard.GameBoard;
+import se.chalmers.dryleafsoftware.androidrally.model.robots.Robot;
+
+public class GameModel {
+	
+	private GameBoard gameBoard;
+	private List<Robot> robots;
+	private Deck deck;
+	
+	public GameModel(int nbrOfPlayers) {
+		gameBoard = new GameBoard(12, 16);
+		for (int i = 0; i < nbrOfPlayers; i++) {
+			robots.add(new Robot(i, 14));
+		}
+	}
 }
