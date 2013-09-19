f8fadc69dae3f234275f617aa76e982a831573c4
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-19 13:54:33 -0700

    Added GameController and basic stuff

---
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/controller/GameController.java
@@ -0,0 +1,11 @@
+package se.chalmers.dryleafsoftware.androidrally.controller;
+
+import se.chalmers.dryleafsoftware.androidrally.model.gameModel.GameModel;
+
+public class GameController {
+	GameModel gameModel;
+	
+	public GameController(int nbrOfPlayers) {
+		gameModel = new GameModel(nbrOfPlayers);
+	}
+}
