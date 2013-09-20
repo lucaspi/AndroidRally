21c9e319628f2bb40571b0bed452f3abb41bc0b1
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-20 09:59:01 -0700

    Added test

--- AndroidRally/test/se/chalmers/dryleafsoftware/androidrally/model/gameBoard/GameBoardTest.java
+++ AndroidRally/test/se/chalmers/dryleafsoftware/androidrally/model/gameBoard/GameBoardTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class GameBoardTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateBoard() {
+		String[][] map = new String();
+		GameBoard gameBoard = new GameBoard(map);
 	}
 
 }
