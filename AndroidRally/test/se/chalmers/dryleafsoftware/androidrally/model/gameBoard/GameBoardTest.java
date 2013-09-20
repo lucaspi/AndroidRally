d71c3548be504f29fc388bf93077b15227d06096
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-20 09:54:00 -0700

    Created uncompleted GameBoardTest

---
+++ AndroidRally/test/se/chalmers/dryleafsoftware/androidrally/model/gameBoard/GameBoardTest.java
@@ -0,0 +1,14 @@
+package se.chalmers.dryleafsoftware.androidrally.model.gameBoard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameBoardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
