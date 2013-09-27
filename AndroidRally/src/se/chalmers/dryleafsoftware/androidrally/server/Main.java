400849ed9f16984ac2bdc4049651d9d5b40cde97
Martin Hermansson <martinhermansson92@gmail.com>
2013-09-27 10:46:38 +0200

    Added temporary main class

---
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/server/Main.java
@@ -0,0 +1,11 @@
+package se.chalmers.dryleafsoftware.androidrally.server;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("HELLO WORLD!!");
+	}
+}
