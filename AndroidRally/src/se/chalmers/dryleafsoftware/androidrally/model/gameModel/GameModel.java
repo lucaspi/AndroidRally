ba901dcfad1a60c3dcf978c639579c125b48f70f
Martin Calleberg <martin.calleberg@gmail.com>
2013-10-04 10:04:56 +0200

    git fix

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameModel/GameModel.java
@@ -42,7 +42,7 @@ public class GameModel {
 	private boolean isGameOver;
 
 	private static final String testMap = "yxxxxxx16x16xxxxx5xxx36:37xyxx06xxx27x37:06xxxx12xxxxx78:16x27yxxxxx27x06xxxxxxx06xx58:16xyx06xxxxxxx32xx22xx4xxx38xyx06xxxxx27x07:26:06x1xxxxx06xx16xyx06xx26x26x27x06xx103x103x103x103x103x07xx18:16xyxxx16x16xxxx203x203x203x203x203xxx28:16xyxxxxxx16x07:06x1xxxxx06xxxyxxx06xxxxxxxxx14xxx48:16xyxxx06xxxx16xx16xxxx06xx68:16xyxxxxxxx17:16xx16:37xxxxxx88xyxxxxxxxxxxxx5xxx57x";
-	
+
 	/**
 	 * Creates a game board of size 12x16 tiles. Also creates robots based
 	 * on the amount of players. Creates a deck with cards that is shuffled.
