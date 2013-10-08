b43cdac8d2782292f8bac2655396b9b10ce1f33a
Martin Hermansson <martinhermansson92@gmail.com>
2013-10-08 11:12:59 +0200

    Deleted turning conveyor belt functionality
    Lack of time

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameBoard/ConveyorBelt.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/model/gameBoard/ConveyorBelt.java
@@ -1,7 +1,5 @@
 package se.chalmers.dryleafsoftware.androidrally.model.gameBoard;
 
-import se.chalmers.dryleafsoftware.androidrally.model.cards.TurnType;
-import se.chalmers.dryleafsoftware.androidrally.model.gameModel.GameModel;
 import se.chalmers.dryleafsoftware.androidrally.model.robots.Robot;
 
 /**
@@ -13,22 +11,6 @@ import se.chalmers.dryleafsoftware.androidrally.model.robots.Robot;
 public class ConveyorBelt implements BoardElement{
 	private int travelDistance;
 	private int direction;
-	private int turn;
-	
-	/**
-	 * The conveyor belt will not affect the robot's direction.
-	 */
-	public static final int NO_TURN = 0;
-	
-	/**
-	 * The conveyor belt will make a right turn for the robot.
-	 */
-	public static final int TURN_RIGHT = 1;
-	
-	/**
-	 * The conveyor belt will make a left turn for the robot.
-	 */
-	public static final int TURN_LEFT = 2;
 	
 	/**
 	 * Creates a new conveyor belt with the specified travelDistance and direction.
@@ -37,19 +19,8 @@ public class ConveyorBelt implements BoardElement{
 	 * @param direction the direction the robot will be moved in
 	 */
 	public ConveyorBelt(int travelDistance, int direction){
-		this(travelDistance, direction, NO_TURN);
-	}
-	
-	/**
-	 * Creates a new conveyor belt with the specified travelDistance, direction and turn.
-	 * @param travelDistance the distance the robot should be moved.
-	 * @param direction the direction the robot will be moved in
-	 * @param turn in which direction (or none) the robot should turn. (0 = no turn, 1 = right turn, 2 = left turn)
-	 */
-	public ConveyorBelt(int travelDistance, int direction, int turn){
 		this.travelDistance = travelDistance;
 		this.direction = direction;
-		this.turn = turn;
 	}
 	
 	/**
@@ -58,15 +29,7 @@ public class ConveyorBelt implements BoardElement{
 	 */
 	@Override
 	public void action(Robot robot) {
-		if(turn == TURN_LEFT){
-			robot.turn(TurnType.LEFT);
-			robot.move(1, (direction + 3) % 4);
-		} else if(turn == TURN_RIGHT){
-			robot.turn(TurnType.RIGHT);
-			robot.move(1, (direction + 1) % 4);
-		} else {
-			robot.move(1, direction);
-		}
+		robot.move(1, direction);
 	}
 
 	@Override
@@ -81,5 +44,4 @@ public class ConveyorBelt implements BoardElement{
 	public int getTravelDistance() {
 		return travelDistance;
 	}
-
 }
