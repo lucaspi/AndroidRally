08d2fc765d0851593cf0282ab83811f62980cbaa
Martin Calleberg <martin.calleberg@gmail.com>
2013-10-13 18:36:24 +0200

    Added dividers between all players' info

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/controller/GameController.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/controller/GameController.java
@@ -34,6 +34,8 @@ public class GameController implements PropertyChangeListener {
 	private int cardTimerSeconds;
 
 	public GameController(int nbrOfHumanPlayers, int nbrOfBots, int hoursEachRound, int cardTimerSeconds, String map) {
+		System.out.println("Bots: " + nbrOfBots);
+		System.out.println("Humans: " + nbrOfHumanPlayers);
 		this.nbrOfHumanPlayers = Math.min(nbrOfHumanPlayers, 8); //So that no one can send in corrupt values.
 		this.nbrOfHumanPlayers = Math.max(this.nbrOfHumanPlayers, 1); //1-8 players and no more than 8 robots incl. bots.
 		this.nbrOfBots = Math.min(nbrOfBots, 8 - this.nbrOfHumanPlayers);
