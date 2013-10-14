84d8d75a7e408718c665f56343d3a4bba7a9cbce
Martin Calleberg <martin.calleberg@gmail.com>
2013-10-14 17:35:58 +0200

    Fixed

--- AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/libgdx/Client.java
+++ AndroidRally/src/se/chalmers/dryleafsoftware/androidrally/libgdx/Client.java
@@ -308,7 +308,7 @@ public class Client {
 		for(int i = 0; i < Integer.parseInt(controller.getNbrOfRobots()); i++) {
 			RobotView robot = new RobotView(i, new TextureRegion(texture, i * 64, 448, 64, 64),
 					new LaserView(new TextureRegion(texture, 64 * i, 384, 64, 64), 0), 
-					"Player " + i);
+					"Player " + (i + 1));
 			if(dockPositions != null) {
 				robot.setPosition(dockPositions[i].x, dockPositions[i].y);
 			}
